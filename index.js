447b1372133e78573141bbe199a5f0ef6d374fbf
Oleksandr Korniiychuk <adissk85@gmail.com>
2024-02-11 15:43:47 +0100

    fix

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
     case "get":
       const contact = await contacts.getContactById(id);
-      return console.log(contact);
+      return console.table(contact);
 
     case "add":
       const addContact = await contacts.addContact(name, email, phone);
-      return console.log(addContact);
+      return console.table(addContact);
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
-      return console.log(removeContact);
+      return console.table(removeContact);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-console.table(invokeAction(options));
+invokeAction(options);
